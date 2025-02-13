598c76cac6d1c1ab0d6706d57134a404fc0cf280
Ashraf <shaik.ashraf2003@gmail.com>
2025-02-13 12:42:17 +0530

    smtg

--- backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
+++ backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("internp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ef69877ba4eb65a4b84a64f86ce3ad98f68474")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69413a131b28478ece490213ba9ed38876263845")]
 [assembly: System.Reflection.AssemblyProductAttribute("internp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("internp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
