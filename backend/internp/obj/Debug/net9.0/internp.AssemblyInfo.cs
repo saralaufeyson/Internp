d8cc0b6d8f4b1d8a70c15cc2f5c918ba788cfe8c
dkneelam <dkneelam786@gmail.com>
2025-02-14 12:53:30 +0530

    CSS Added

--- backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
+++ backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("internp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc19ffaaa80daa6f1004a1ace9c050d2e59b5a25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+583539d0e192f96790a26d35a94efa1d7c455d7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("internp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("internp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
