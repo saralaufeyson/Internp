ee2186457d4ca47c0e2b6bf17af30cf5a3477ff8
dkneelam <dkneelam786@gmail.com>
2025-02-14 12:10:28 +0530

    dropdown checkbox added in mentor

--- backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
+++ backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("internp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33816706b00741bbab281e78766d3d02070004ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed58aa5c755feff40e8610e8e6e65a233ae818e")]
 [assembly: System.Reflection.AssemblyProductAttribute("internp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("internp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
