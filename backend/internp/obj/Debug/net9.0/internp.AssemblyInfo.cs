ea3ee5cacb2611e1d1d81aee4d41ff4c670eefd3
Ashraf <shaik.ashraf2003@gmail.com>
2025-02-21 10:14:59 +0530

    smtg

--- backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
+++ backend/internp/obj/Debug/net9.0/internp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("internp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17f665f63e010cd5c7c95f8dd29563ddf8f6b9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380cd2ac435429e16394a045faf46060604db4de")]
 [assembly: System.Reflection.AssemblyProductAttribute("internp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("internp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- main/src/app/app.config.ts
+++ main/src/app/app.config.ts
@@ -1,19 +1,7 @@
-import {
-  ApplicationConfig,
-  provideZoneChangeDetection,
-  importProvidersFrom,
-} from '@angular/core';
-import {
-  HttpClient,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, } from '@angular/core';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi, } from '@angular/common/http';
 import { routes } from './app.routes';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-} from '@angular/router';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling, } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration } from '@angular/platform-browser';
 // import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
@@ -29,32 +17,23 @@ import { NgScrollbarModule } from 'ngx-scrollbar';
 //Import all material modules
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimations } from "@angular/platform-browser/animations";
 
 // export function HttpLoaderFactory(http: HttpClient): any {
 //   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 // }
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      }),
-      withComponentInputBinding()
-    ),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideClientHydration(),
-    provideAnimationsAsync(),
-    importProvidersFrom(
-      FormsModule,
-      ReactiveFormsModule,
-      MaterialModule,
-      TablerIconsModule.pick(TablerIcons),
-      NgScrollbarModule,
-     
-    ),
-  ],
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes, withInMemoryScrolling({
+			scrollPositionRestoration: 'enabled',
+			anchorScrolling: 'enabled',
+		}), withComponentInputBinding()),
+		provideHttpClient(withInterceptorsFromDi()),
+		provideClientHydration(),
+		provideAnimationsAsync(),
+		importProvidersFrom(FormsModule, ReactiveFormsModule, MaterialModule, TablerIconsModule.pick(TablerIcons), NgScrollbarModule),
+		provideAnimations()
+	],
 };
