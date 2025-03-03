abf0e7c36608110d07ffe92ef8120faa63143802
dkneelam <dkneelam786@gmail.com>
2025-03-03 17:40:34 +0530

    removed mylearn service

--- InternPortalAPI/obj/Debug/net9.0/InternPortal.AssemblyInfo.cs
+++ InternPortalAPI/obj/Debug/net9.0/InternPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InternPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62ee2d3e505e23b3198085b5b2101d2e8120d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b2438535eb2ee00d10f453857b28be8fba9b1ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("InternPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InternPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
