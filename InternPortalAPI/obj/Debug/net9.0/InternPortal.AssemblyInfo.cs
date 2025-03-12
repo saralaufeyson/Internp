cd395739c18cc292fc1881fbd84d7dde1cb1664b
dkneelam <dkneelam786@gmail.com>
2025-03-12 15:30:18 +0530

    intern review

--- InternPortalAPI/obj/Debug/net9.0/InternPortal.AssemblyInfo.cs
+++ InternPortalAPI/obj/Debug/net9.0/InternPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InternPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbaa52f5d96304522b3822a1e74e2935cf7f22b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c257ff22705977459ccb047f80e1403d591e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("InternPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InternPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
